02912ad2b7dee73c5a3fea4c687d61850d24ca10
JohnPH75 <johnhussain75@gmail.com>
2016-06-29 21:58:16 -0400

    calculator

--- Open-Me/FISViewController.h
+++ Open-Me/FISViewController.h
@@ -8,6 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FISViewController : UIViewController
+typedef enum{ Plus,Minus,Multiply,Divide} CalcOperation;
+
+@interface FISViewController : UIViewController{
+    
+    IBOutlet UITextField *display;
+//    IBOutlet UIButton *cbutton;
+    NSString *storage;
+    CalcOperation operation;
+}
+
+- (IBAction) button1;
+- (IBAction) button2;
+- (IBAction) button3;
+- (IBAction) button4;
+- (IBAction) button5;
+- (IBAction) button6;
+- (IBAction) button7;
+- (IBAction) button9;
+- (IBAction) button0;
+- (IBAction) plusbutton;
+- (IBAction) equalsbutton;
+- (IBAction) clearDisplay;
 
 @end
--- Open-Me/FISViewController.m
+++ Open-Me/FISViewController.m
@@ -8,6 +8,8 @@
 
 #import "FISViewController.h"
 
+
+
 @interface FISViewController ()
 
 @end
@@ -15,9 +17,96 @@
 
 @implementation FISViewController
 
+- (IBAction) clearDisplay {
+    display.text = @"0";
+}
+
+- (IBAction) button1 {
+    display.text=[NSString stringWithFormat:@"%@1",display.text];
+}
+- (IBAction) button2 {
+    display.text=[NSString stringWithFormat:@"%@2",display.text];
+}
+- (IBAction) button3 {
+    display.text=[NSString stringWithFormat:@"%@3",display.text];
+}
+
+- (IBAction) button4 {
+    display.text=[NSString stringWithFormat:@"%@4",display.text];
+}
+
+- (IBAction) button5 {
+    display.text=[NSString stringWithFormat:@"%@5",display.text];
+}
+
+- (IBAction) button6 {
+    display.text=[NSString stringWithFormat:@"%@6",display.text];
+}
+
+- (IBAction) button7 {
+    display.text=[NSString stringWithFormat:@"%@7",display.text];
+}
+
+- (IBAction) button8 {
+    display.text=[NSString stringWithFormat:@"%@8",display.text];
+}
+
+- (IBAction) button9 {
+    display.text=[NSString stringWithFormat:@"%@9",display.text];
+}
+
+- (IBAction) button0 {
+    display.text=[NSString stringWithFormat:@"%@0",display.text];
+}
+
+- (IBAction) plusbutton {
+    operation = Plus;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction) minusbutton {
+    operation = Minus;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction) multiplybutton {
+    operation = Multiply;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction) dividebutton {
+    operation = Divide;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction) equalsbutton {
+    NSString *val = display.text;
+    switch(operation) {
+        case Plus :
+            display.text= [NSString stringWithFormat:@"%qi",[val longLongValue]+[storage longLongValue]];
+            break;
+        case Minus:
+            display.text= [NSString stringWithFormat:@"%qi",[storage longLongValue]-[val longLongValue]];
+            break;
+        case Divide:
+            display.text= [NSString stringWithFormat:@"%qi",[storage longLongValue]/[val longLongValue]];
+            break;
+        case Multiply:
+            display.text= [NSString stringWithFormat:@"%qi",[val longLongValue]*[storage longLongValue]];
+            break;
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+     display.text = @"0";
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
